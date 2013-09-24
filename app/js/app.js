53e943a7c88b23199413da5dba08702544e93d20
davidsmith2 <davidsmith2@gmail.com>
2013-09-24 17:48:14 -0400

    cleaner instantiations

--- app/js/app.js
+++ app/js/app.js
@@ -18,34 +18,42 @@ function ($, _, Backbone, config, Alert, Search, Searches, Students, Uploads, Ma
 
     var App = function () {
 
+        // collections
         this.collections.students = new Students();
-        this.collections.students.add(config.students);
-
         this.collections.searches = new Searches();
-        this.collections.searches.on('sync', this.showSearchResultsView, this).fetch({reset: true});
-
         this.collections.uploads = new Uploads();
-        this.collections.uploads.on('sync', this.showUploadView, this).fetch({reset: true});
 
+        // models
         this.models.search = new Search();
 
-        $('#geographyZipCodesManualContainer').replaceWith(new ManualView(this.models.search, new Alert()).render().el);
-        $('#geographySelectionsContainer').replaceWith(new SelectionsView(this.models.search).render().el);
+        // views
+        this.views.uploadView = new UploadView(this.collections.uploads, this.models.search, new Alert());
+        this.views.manualView = new ManualView(this.models.search, new Alert());
+        this.views.searchResultsView = new SearchResultsView(this.models.search, this.collections.students, this.collections.searches);
+        this.views.selectionsView = new SelectionsView(this.models.search);
+
+        // populate collections and define callbacks
+        this.collections.students.add(config.students);
+        this.collections.searches.on('sync', this.showSearchResultsView, this).fetch({reset: true});
+        this.collections.uploads.on('sync', this.showUploadView, this).fetch({reset: true});
+
+        // render initial views
+        $('#geographyZipCodesManualContainer').replaceWith(this.views.manualView.render().el);
+        $('#geographySelectionsContainer').replaceWith(this.views.selectionsView.render().el);
+
+        console.log(this);
     };
 
     App.prototype = {
-        models: {},
         collections: {},
+        models: {},
         views: {},
-
         showUploadView: function () {
-            $('#geographyZipCodesUploadContainer').replaceWith(new UploadView(this.collections.uploads, this.models.search, new Alert()).render().el);
+            $('#geographyZipCodesUploadContainer').replaceWith(this.views.uploadView.render().el);
         },
-
         showSearchResultsView: function () {
-            $('#searchResultsContainer').replaceWith(new SearchResultsView(this.models.search, this.collections.students, this.collections.searches).render().el);
+            $('#searchResultsContainer').replaceWith(this.views.searchResultsView.render().el);
         }
-
     };
 
     return App;
